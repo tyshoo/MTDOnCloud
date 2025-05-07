b4aa9d3f18b38c0ae7224c59d921dd14d14d5633
tyshoo <54097381+tyshoo@users.noreply.github.com>
2025-05-08 00:42:54 +0800

    Create variables.tf

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,4 @@
+variable "environment" { type = string }
+variable "vpc_cidr"    { type = string }
+variable "public_subnet_cidrs"  { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
